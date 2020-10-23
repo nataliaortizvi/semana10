368c69a962870f894567b06b358932769a2054bb
nataliaortizvi <natiorvi00@gmail.com>
2020-10-23 18:37:50 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//instancio 
+const ids = document.getElementById('ids');
+const nombres = document.getElementById('nombres');
+const btRegistrar = document.getElementById('btRegistrar');
+const idDos = document.getElementById('idDos');
+const btVotar = document.getElementById('btVotar');
+const btCandidatos = document.getElementById('btCandidatos');
+const btVotaciones = document.getElementById('btVotaciones');
+
+var database = firebase.database();
+
+//declarar functions
+meter = () => {
+    let d = ids.value;
+    let n = nombres.value;
+    let r = btRegistrar.value;
+    let i = idDos.value;
+    let v = btVotar.value;
+    let bc = btCandidatos.value;
+    let bv = btVotaciones.value;
+}
